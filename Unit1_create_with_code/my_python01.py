ad99bc93f2ebd2be13fecb9f48db6e30f79e3db7
JackKozera <123520134+JackKozera@users.noreply.github.com>
2023-01-27 12:16:28 +1000

    Create my_python01.py

---
+++ Unit1_create_with_code/my_python01.py
@@ -0,0 +1,11 @@
+# this is a comment
+""""
+multi line comment
+dssddssd
+""""
+name = input("what is your name?")
+age = input("What is your age?")
+
+age = 2023 + age
+
+print("hi, " +name "your age in 20 years will be ", age)
